9c9a4304a9a2594d4e25c5d8cac66ed16f3915a6
s-ariga <seiichi.ariga@gmail.com>
2019-09-07 23:30:12 +0900

    表示の修正

--- lib/main.dart
+++ lib/main.dart
@@ -7,37 +7,37 @@ void main() {
   // アプリ起動
   runApp(new ShootApp());
 }
+
 class ShootApp extends StatelessWidget {
 // アプリのセットアップ
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      // 画面遷移を定義
+        // 画面遷移を定義
 
-      // その他の変数を設定
-      title: 'Shooting App',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: const Color(0xFF2196f3),
-        accentColor: const Color(0xFF2196f3),
-        canvasColor: const Color(0xFFfafafa),
-      ),
-      home: new ShootHome(),
-      routes: {
-        ResultPage.routeName: (context) => ResultPage(),
-      }
-    );
+        // その他の変数を設定
+        title: 'Shooting App',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: const Color(0xFF2196f3),
+          accentColor: const Color(0xFF2196f3),
+          canvasColor: const Color(0xFFfafafa),
+        ),
+        home: new ShootHome(),
+        routes: {
+          ResultPage.routeName: (context) => ResultPage(),
+        });
   }
 }
 
 class ShootHome extends StatefulWidget {
   ShootHome({Key key}) : super(key: key);
+
   @override
   _ShootHomeState createState() => new _ShootHomeState();
 }
 
 class _ShootHomeState extends State<ShootHome> {
-
   Shots _shots;
   int _scorePredict;
   String _dirPredict;
@@ -45,7 +45,7 @@ class _ShootHomeState extends State<ShootHome> {
   String _dirActual;
 
   final int _scoreInit = 0;
-  final String _dirInit = "*";
+  final String _dirInit = "＊";
 
   _ShootHomeState() {
     _shots = new Shots();
@@ -57,19 +57,19 @@ class _ShootHomeState extends State<ShootHome> {
 
   Route _createRoute() {
     return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => ResultPage(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var begin = Offset(0.0, 1.0);
-        var end = Offset.zero;
-        var tween = Tween(begin: begin, end: end);
-        var offsetAnimation = animation.drive(tween);
-        return SlideTransition(
-          position: offsetAnimation,
-          child: child,
-        );
-      }
-    );
+        pageBuilder: (context, animation, secondaryAnimation) => ResultPage(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          var begin = Offset(0.0, 1.0);
+          var end = Offset.zero;
+          var tween = Tween(begin: begin, end: end);
+          var offsetAnimation = animation.drive(tween);
+          return SlideTransition(
+            position: offsetAnimation,
+            child: child,
+          );
+        });
   }
+
   @override
   Widget build(BuildContext context) {
     // return Scaffoldでページを返す
@@ -77,48 +77,57 @@ class _ShootHomeState extends State<ShootHome> {
       appBar: new AppBar(
         title: new Text('予告射撃'),
       ),
-      body: Center(
+      body: Container(
         child: Column(
           children: <Widget>[
             // 丸数字
-            Text('${_shots.shots+1}発目',
-            style: new TextStyle(
-              fontSize: 32.0,
-              fontWeight: FontWeight.w600
-            )
-            ),
+            Text('${_shots.shots + 1}発目',
+                style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600)),
             // 文字か画像
             Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text('予告'),
-                Text('結果'),
-              ]
-            ),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('予告',
+                      style: TextStyle(
+                          fontSize: 32.0, fontWeight: FontWeight.w600)),
+                  Text('結果',
+                      style: TextStyle(
+                          fontSize: 32.0, fontWeight: FontWeight.w600)),
+                ]),
             Row(
-              children: <Widget>[
-                _buildScoreButtons(),
-                _buildDirectionButtons(),
-                _buildActualScoreButtons(),
-                _buildActualDirectionButtons(),
-              ]
-            ),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildScoreButtons(),
+                  _buildDirectionButtons(),
+                  _buildActualScoreButtons(),
+                  _buildActualDirectionButtons(),
+                ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <
+                Widget>[
+              Text('$_scorePredict',
+                  style:
+                      TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600)),
+              Text(_dirPredict,
+                  style:
+                      TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600)),
+              Text('$_scoreActual',
+                  style:
+                      TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600)),
+              Text(_dirActual,
+                  style:
+                      TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600)),
+            ]),
             Row(
-              children: <Widget>[
-                Text('$_scorePredict'),
-                Text(_dirPredict),
-                Text('$_scoreActual'),
-                Text(_dirActual),
-              ]
-            ),
-            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 RaisedButton.icon(
                   icon: Icon(
                     Icons.add_circle_outline,
                     color: Colors.white,
                   ),
-                  label: Text('つぎ!'),
+                  label: Text('つぎ!',style:
+                  TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600)),
                   color: Colors.green,
                   textColor: Colors.white,
                   onPressed: _nextPressed,
@@ -128,7 +137,8 @@ class _ShootHomeState extends State<ShootHome> {
                     Icons.assessment,
                     color: Colors.white,
                   ),
-                  label: Text('結果!'),
+                  label: Text('結果!',style:
+                  TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600)),
                   color: Colors.red,
                   textColor: Colors.white,
                   onPressed: () {
@@ -147,7 +157,7 @@ class _ShootHomeState extends State<ShootHome> {
   }
 
   void _nextPressed() {
-    if(_scoreActual != 0 && _dirActual != "?") {
+    if (_scoreActual != 0 && _dirActual != "?") {
       setState(() {
         _shots.shots++;
         _shots.pushResult(_scorePredict, _dirPredict, _scoreActual, _dirActual);
@@ -160,7 +170,7 @@ class _ShootHomeState extends State<ShootHome> {
   }
 
   static const score = [10, 9, 8, 7, 6];
-  static const direction = ['↑','↗','→','↘','↓','↙','←','↖'];
+  static const direction = ['↑', '↗', '→', '↘', '↓', '↙', '←', '↖'];
 
   Widget _buildScoreButtons() {
     return Column(
@@ -171,22 +181,20 @@ class _ShootHomeState extends State<ShootHome> {
   List<Widget> _buildScoreButton() {
     List<Widget> buttons = [];
     for (var s in score) {
-      buttons.add(
-          RaisedButton(
-            child: Text("$s"),
-            color: Colors.white,
-            shape: CircleBorder(
-              side: BorderSide(
-                color: Colors.black,
-                width: 1.0,
-                style: BorderStyle.solid,
-              ),
-            ),
-            onPressed: () {
-              setState(() => _scorePredict = s);
-            },
-          )
-      );
+      buttons.add(RaisedButton(
+        child: Text("$s"),
+        color: Colors.white,
+        shape: CircleBorder(
+          side: BorderSide(
+            color: Colors.black,
+            width: 1.0,
+            style: BorderStyle.solid,
+          ),
+        ),
+        onPressed: () {
+          setState(() => _scorePredict = s);
+        },
+      ));
     }
     return buttons;
   }
@@ -227,22 +235,20 @@ class _ShootHomeState extends State<ShootHome> {
   List<Widget> _buildActualScoreButton() {
     List<Widget> buttons = [];
     for (var s in score) {
-      buttons.add(
-          RaisedButton(
-            child: Text("$s"),
-            color: Colors.white,
-            shape: CircleBorder(
-              side: BorderSide(
-                color: Colors.black,
-                width: 1.0,
-                style: BorderStyle.solid,
-              ),
-            ),
-            onPressed: () {
-              setState(() => _scoreActual = s);
-            },
-          )
-      );
+      buttons.add(RaisedButton(
+        child: Text("$s"),
+        color: Colors.white,
+        shape: CircleBorder(
+          side: BorderSide(
+            color: Colors.black,
+            width: 1.0,
+            style: BorderStyle.solid,
+          ),
+        ),
+        onPressed: () {
+          setState(() => _scoreActual = s);
+        },
+      ));
     }
     return buttons;
   }
@@ -274,6 +280,3 @@ class _ShootHomeState extends State<ShootHome> {
     return buttons;
   }
 }
-
-
-
--- lib/result.dart
+++ lib/result.dart
@@ -10,54 +10,44 @@ class ResultPage extends StatelessWidget {
     final Shots shots = ModalRoute.of(context).settings.arguments;
 
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('結果! 　${shots.results.length}発'),
-      ),
-      body: new Row(
-          children: <Widget> [
+        appBar: new AppBar(
+          title: new Text('結果! 　${shots.results.length}発'),
+        ),
+        body: new Row(
+          children: <Widget>[
             Expanded(
-            child: ListView.builder(
-            itemCount: shots.results.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-            title: Text("${index+1}発目  ${shots.results[index].toString()}"),
-          );
-        }
-      ),
+              child: ListView.builder(
+                  itemCount: shots.results.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                          "${index + 1}発目  ${shots.results[index].toString()}",
+                          style:
+                          TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400)),
+                    );
+                  }),
             ),
             Expanded(
                 child: new Column(
-                    children: <Widget> [
-                      new Text('点数的中 ${(shots.scoreAtari()*100).toStringAsFixed(1)}%'),
-                      new Text('矢印的中 ${(shots.dirAtari()*100).toStringAsFixed(1)}%'),
-                      new Text('合計 ${shots.totalScore()}'),
-                      new Text('平均 ${(shots.totalScore()/shots.results.length).toStringAsFixed(1)}')
-                    ]
-                )
-            ),
-        ],
-      )
-      /*
-      body: new Row(
-        children: <Widget> [
-          ListView.builder(
-            itemCount: 20,
-            itemBuilder: (context, index) {
-              return ListTile(
-                  title: Text("$index"),
-              );
-            },
-          ),
-          new Column(
-            children: <Widget> [
-              new Text('\%'),
-              new Text('点'),
-              new Text('点')
-            ]
-          )
-        ]
-      )
-        */
-    );
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                  new Text(
+                      '点数的中 ${(shots.scoreAtari() * 100).toStringAsFixed(1)}%',
+                      style:
+                      TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600)),
+                  new Text(
+                      '方向的中 ${(shots.dirAtari() * 100).toStringAsFixed(1)}%',
+                      style:
+                      TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600)),
+                  new Text('合計 ${shots.totalScore()}',
+                      style:
+                      TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600)),
+                  new Text(
+                      '平均 ${(shots.totalScore() / shots.results.length).toStringAsFixed(1)}',
+                      style:
+                      TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600))
+                ])),
+          ],
+        ));
   }
 }
